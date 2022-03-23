82767d4490ca3fe73510d02da935526c31ab8212
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2022-03-24 00:59:06 +0600

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import bg from '../images/Group.svg'
-import './Home.css'
+import React from 'react';
+import bg from '../images/Group.svg';
+import background from "../images/logo.png";
+import './Home.css';
 export const Home = () => {
   return (
-    <div className=''>
-        <div className='row hero-color'>
-          <div className='col-md-8 hero me-5 mt-5'>
-<img src={bg} className='w-50 me-5 mt-6'/>
+    <div className='container-fluid'>
+        <div className='row '>
+          <div className='col-md-8 hero'>
+<img src={bg} className='io'/>
           </div>
-          <div className='col-md-4 hero'>
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          <div className='col-md-4 lo'>
+<img src={background} className='i'/>
+         <h3 className="t" >Explore The Best Tours... Hurry up!!</h3>
+  <input type="email" class="form-control oo" id="exampleFormControlInput1" placeholder="name@example.com"/>
+  <input type="email" class="form-control oo" id="exampleFormControlInput1" placeholder="name@example.com"/>
 
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-
+<p >Don't have an account?  Sign Up</p>
           </div>
 
         </div>
